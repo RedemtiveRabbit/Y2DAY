749bc88cf420048bcf4ebf515fc57d673ec8f035
Flappyy <flappydiscord@gmail.com>
2025-07-10 12:47:54 -0500

    script for attack and new layer

--- Assets/Swing.cs
+++ Assets/Swing.cs
@@ -1,20 +1,30 @@
+using System.Collections;
 using UnityEngine;
 
 public class Swing : MonoBehaviour
 {
-    public int Time = 0;
+    public int time = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        gameObject.SetActive(false);
         
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-       if (Input.GetButtonDown("Fire1") && )
+        if (Input.GetButtonDown("Fire1"))
         {
-            
+            StartCoroutine(BoxRoutine());
         }
+    }
+
+    private IEnumerator BoxRoutine()
+    {
+        GetComponent<CapsuleCollider2D>().enabled = true;
+        yield return new WaitForSeconds(time);
+        GetComponent<CapsuleCollider2D>().enabled = false;
+
     }
 }
