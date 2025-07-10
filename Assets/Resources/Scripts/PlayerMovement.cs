94d13ada08c0c9391c07ca4d4d01f86390c0f5a5
Jonathan Saunders <jonathanedsaunders@gmail.com>
2025-07-10 10:43:39 -0400

    Initial Commit

---
+++ Assets/Resources/Scripts/PlayerMovement.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D body;
+
+    float horizontal;
+    float vertical;
+    float moveLimiter = 0.7f;
+    public Sprite up;
+    public Sprite down;
+    public Sprite left;
+    public Sprite right;
+
+    public float moveSpeed;
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+
+        GetDirection(); //uses input to figure out what direction the player should move
+        LimitDiag(); //limits the diagonal movement so that it isn't faster than horizontal or vertical
+        ApplyMovement(); //applies direction information to effect the player's linear velocity
+        SetSpriteDirection(); //makes it so the sprite faces the direction it's moving
+
+    }
+
+    void GetDirection()
+    {
+        // Gives a value between -1 and 1 depending on direction
+        horizontal = Input.GetAxis("Horizontal"); // -1 is left
+        vertical = Input.GetAxis("Vertical"); // -1 is down
+    }
+
+    void LimitDiag()
+    {
+        if (horizontal != 0 && vertical != 0) // Check for diagonal movement
+        {
+            // limit movement speed diagonally
+            horizontal *= moveLimiter;
+            vertical *= moveLimiter;
+        }
+    }
+
+    void ApplyMovement()
+    {
+        body.linearVelocity = new Vector2(horizontal * moveSpeed, vertical * moveSpeed); // sets linear velocity as a Vector2, multiplying direction by the moveSpeed
+    }
+
+    void SetSpriteDirection()
+    {
+        SpriteRenderer sprite = body.GetComponent<SpriteRenderer>();
+
+        //checks the velocities and sets sprites accordingly
+        if (vertical > 0)
+        {
+            sprite.sprite = up;
+        }
+        else if(vertical < 0)
+        {
+            sprite.sprite = down;
+        }
+        else if(horizontal > 0)
+        {
+            sprite.sprite = right;
+        }
+        else if(horizontal < 0)
+        {
+            sprite.sprite = left;
+        }
+    }
+}
