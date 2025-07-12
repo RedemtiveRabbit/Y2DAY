256a8d921a6c44b64093b3e662c587b745ab0bd6
Jonathan Saunders <jonathanedsaunders@gmail.com>
2025-07-12 13:20:43 -0400

    fixed swing for the third time

--- Assets/Resources/Scripts/Swing.cs
+++ Assets/Resources/Scripts/Swing.cs
@@ -6,13 +6,17 @@ public class Swing : MonoBehaviour
     public float time = 0;
     public PlayerMovement playerMovement;
     private int direction;
-    
-    
+    public float rightOffset;
+    public float leftOffset;
+    public float upOffset;
+    public float downOffset;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
 
-        GetComponent<BoxCollider2D>().enabled = false;
+        GetComponent<CircleCollider2D>().enabled = false;
         //attack collider starts deactivated
     }
 
@@ -32,7 +36,6 @@ public class Swing : MonoBehaviour
     }
     void OnTriggerEnter2D(Collider2D other)
     {
-        print("hi");
         if (other?.GetComponent<Health>() is Health target)
         {
             //this isnt happening
@@ -44,35 +47,35 @@ public class Swing : MonoBehaviour
     }
     private IEnumerator BoxRoutine()
     {
-        GetComponent<BoxCollider2D>().enabled = true;
+        GetComponent<CircleCollider2D>().enabled = true;
         //make collider visible
         if (direction == 1)
         {
-            transform.localPosition = new Vector3(0, .1f, 0);
+            transform.localPosition = new Vector3(0, upOffset, 0);
             yield return new WaitForSeconds(time);
             transform.localPosition = new Vector3(0, 0, 0);
         }
         else if (direction == 2)
         {
-            transform.localPosition = new Vector3(.1f, 0, 0);
+            transform.localPosition = new Vector3(rightOffset, 0, 0);
             yield return new WaitForSeconds(time);
             transform.localPosition = new Vector3(0, 0, 0);
         }
         else if (direction == 3)
         {
-            transform.localPosition = new Vector3(0, -.12f, 0);
+            transform.localPosition = new Vector3(0, -downOffset, 0);
             yield return new WaitForSeconds(time);
             transform.localPosition = new Vector3(0, 0, 0);
         }
         else if (direction == 4)
         {
-            transform.localPosition = new Vector3(-.1f, 0, 0);
+            transform.localPosition = new Vector3(-leftOffset, 0, 0);
             yield return new WaitForSeconds(time);
             transform.localPosition = new Vector3(0, 0, 0);
         }
         //move collider based on what direction sprite was facing at the time that the attack was activated
         //delay before the collider deactivates
-        GetComponent<BoxCollider2D>().enabled = false;
+        GetComponent<CircleCollider2D>().enabled = false;
         //collider deactivates at the end
     }
 }
