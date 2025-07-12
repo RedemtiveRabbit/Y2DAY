4c7c79d2601eab623d6ca0e0d25764199339e314
Jonathan Saunders <jonathanedsaunders@gmail.com>
2025-07-12 12:23:35 -0400

    Update Swing.cs

--- Assets/Resources/Scripts/Swing.cs
+++ Assets/Resources/Scripts/Swing.cs
@@ -5,6 +5,10 @@ public class Swing : MonoBehaviour
 {
     public float time = 0;
     public PlayerMovement playerMovement;
+    public float leftOffset;
+    public float rightOffset;
+    public float upOffset;
+    public float downOffset;
     private int direction;
     
 
@@ -12,7 +16,7 @@ public class Swing : MonoBehaviour
     void Start()
     {
 
-        GetComponent<BoxCollider2D>().enabled = false;
+        GetComponent<CircleCollider2D>().enabled = false;
 
     }
 
@@ -31,34 +35,34 @@ public class Swing : MonoBehaviour
 
     private IEnumerator BoxRoutine()
     {
-        GetComponent<BoxCollider2D>().enabled = true;
+        GetComponent<CircleCollider2D>().enabled = true;
 
         if (direction == 1)
         {
-            transform.localPosition = new Vector3(0, .1f, 0);
+            transform.localPosition = new Vector3(0, upOffset, 0);
             yield return new WaitForSeconds(time);
             transform.localPosition = new Vector3(0, 0, 0);
         }
         else if (direction == 2)
         {
-            transform.localPosition = new Vector3(.1f, 0, 0);
+            transform.localPosition = new Vector3(rightOffset, 0, 0);
             yield return new WaitForSeconds(time);
             transform.localPosition = new Vector3(0, 0, 0);
         }
         else if (direction == 3)
         {
-            transform.localPosition = new Vector3(0, -.12f, 0);
+            transform.localPosition = new Vector3(0, -downOffset, 0);
             yield return new WaitForSeconds(time);
             transform.localPosition = new Vector3(0, 0, 0);
         }
         else if (direction == 4)
         {
-            transform.localPosition = new Vector3(-.1f, 0, 0);
+            transform.localPosition = new Vector3(-leftOffset, 0, 0);
             yield return new WaitForSeconds(time);
             transform.localPosition = new Vector3(0, 0, 0);
         }
 
-        GetComponent<BoxCollider2D>().enabled = false;
+        GetComponent<CircleCollider2D>().enabled = false;
 
     }
 }
