8b6cfe968c671a156f5b2768fc69265ce2639327
Jonathan Saunders <jonathanedsaunders@gmail.com>
2025-07-13 12:25:59 -0400

    starting trying to add knockback, totally broken

--- Assets/Resources/Scripts/AI_Chase.cs
+++ Assets/Resources/Scripts/AI_Chase.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -5,19 +6,31 @@ public class AI_Chase : MonoBehaviour
 
 {
     public GameObject player;
+    Rigidbody2D body;
     public float speed;
     public float distanceBetween;
     private NavMeshAgent agent;
     private float distance;
+    public PlayerMovement playerMovement;
+    private float direction;
+    public int HP;
+    public float knockBackStrength;
+    public float knockBackTime;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        body = GetComponent<Rigidbody2D>();
         agent.enabled = false;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
+    {
+        FollowPlayer();
+    }
+
+    void FollowPlayer()
     {
         distance = Vector2.Distance(transform.position, player.transform.position);
         Vector2 direction = player.transform.position - transform.position;
@@ -35,5 +48,35 @@ public class AI_Chase : MonoBehaviour
             agent.SetDestination(player.transform.position);
             transform.rotation = Quaternion.Euler(Vector3.forward * angle);
         }
+    }
+
+    IEnumerator KnockBackRoutine()
+    {
+        if (direction == 0.25)
+        {
+            body.linearVelocityY = knockBackStrength;
+            yield return new WaitForSeconds(knockBackTime);
+        }
+        if (direction == 0.5)
+        {
+            body.linearVelocityX = knockBackStrength;
+            yield return new WaitForSeconds(knockBackTime);
+        }
+        if (direction == 0.75)
+        {
+            body.linearVelocityY = -knockBackStrength;
+            yield return new WaitForSeconds(knockBackTime);
+        }
+        if (direction == 1)
+        {
+            body.linearVelocityX = -knockBackStrength;
+            yield return new WaitForSeconds(knockBackTime);
+
+        }
+    }
+
+    public void KnockBack()
+    {
+        StartCoroutine(KnockBackRoutine());
     }
 }
--- Assets/Resources/Scripts/Health.cs
+++ Assets/Resources/Scripts/Health.cs
@@ -1,12 +1,20 @@
+using System;
+using System.Collections;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class Health : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public int HP; 
+    Rigidbody2D body;
+    public PlayerMovement playerMovement;
+    private float direction;
+    public int HP;
+    public float knockBackStrength;
+    public float knockBackTime;
     void Start()
     {
-        
+        body = GetComponent<Rigidbody2D>();
     }
 
     void Die()
@@ -28,11 +36,39 @@ public class Health : MonoBehaviour
         {
             Die();
         }
+        playerMovement.spriteDirection = direction;
 
     }
     public void Defend(int damage)
     {
         HP -= damage;
+        StartCoroutine(KnockBackRoutine());
+        
+    }
+
+    IEnumerator KnockBackRoutine()
+    {
+        if (direction == 0.25)
+        {
+            body.linearVelocityY = knockBackStrength;
+            yield return new WaitForSeconds(knockBackTime);
+        }
+        if (direction == 0.5)
+        {
+            body.linearVelocityX = knockBackStrength;
+            yield return new WaitForSeconds(knockBackTime);
+        }
+        if (direction == 0.75)
+        {
+            body.linearVelocityY = -knockBackStrength;
+            yield return new WaitForSeconds(knockBackTime);
+        }
+        if (direction == 1)
+        {
+            body.linearVelocityX = -knockBackStrength;
+            yield return new WaitForSeconds(knockBackTime);
+
+        }
     }
 
 
