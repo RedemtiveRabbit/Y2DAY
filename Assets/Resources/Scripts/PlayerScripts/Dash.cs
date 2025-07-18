508f02bc2924cc5519c8a9670603699be5e17a86
Flappyy <flappydiscord@gmail.com>
2025-07-18 10:31:07 -0400

    Added invincibility "frames" when hit

--- Assets/Resources/Scripts/PlayerScripts/Dash.cs
+++ Assets/Resources/Scripts/PlayerScripts/Dash.cs
@@ -17,6 +17,7 @@ public class Dash : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        
         body = GetComponent<Rigidbody2D>();
     }
 
@@ -36,8 +37,8 @@ public class Dash : MonoBehaviour
         canDash = false;
         print("dashed");
         audioSource2.Play();
-
-        if(direction == 1)
+        
+        if (direction == 1)
         {
             body.linearVelocityY = dashStrength;
             yield return new WaitForSeconds(dashTime);
@@ -61,8 +62,9 @@ public class Dash : MonoBehaviour
             yield return new WaitForSeconds(dashTime);
             body.linearVelocityX = xVelocity;
         }
-
+        
         dashing = false;
+        
         yield return new WaitForSeconds(coolDown);
         canDash = true;
         
