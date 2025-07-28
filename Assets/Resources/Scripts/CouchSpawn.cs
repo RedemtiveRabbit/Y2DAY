dc966ac0b41928b6e4f1a24f40944d7cb72681e5
Jonathan Saunders <jonathanedsaunders@gmail.com>
2025-07-28 16:23:54 -0400

    bgbgvbgujh

---
+++ Assets/Resources/Scripts/CouchSpawn.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class CouchSpawn : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if(SaveData.current.hasCouch != 1)
+        {
+            this.gameObject.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
