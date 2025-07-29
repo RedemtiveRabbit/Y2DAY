9e0733aad8ae1a580c457bf94d5a81144e7ddd89
Jonathan Saunders <jonathanedsaunders@gmail.com>
2025-07-29 19:29:11 -0400

    as

--- Assets/Resources/Scripts/Enemy stuffs/Beam.cs
+++ Assets/Resources/Scripts/Enemy stuffs/Beam.cs
@@ -5,10 +5,16 @@ public class Beam : MonoBehaviour
     public BoxCollider2D beamBox;
     public GameObject player;
     public bool active;
-    public float beamTime;
-    public float timer;
+    private float beamTime;
+    private float timer;
     public GameObject beam;
     public float rotationSpeed = 5f;
+    public bool charging = false;
+    public bool beaming = false;
+    public float chargeTime;
+    public float beamTimes;
+    public Animator animator;
+    public float alertDistance;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -18,26 +24,33 @@ public class Beam : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        animator.SetBool("Charging", charging);
+        animator.SetBool("Beaming", beaming);
+
         float distance = Vector2.Distance(transform.position, player.transform.position);
         Vector3 direction = player.transform.position - transform.position;
         Quaternion targetRotation = Quaternion.LookRotation(Vector3.forward, direction);
         beam.transform.rotation = Quaternion.RotateTowards(beam.transform.rotation, targetRotation, rotationSpeed * Time.deltaTime * 100f);
-        if (active == false)
+        if (active == false && distance < alertDistance)
         {
+            charging = true;
             timer += Time.deltaTime;
-            if (timer >= 5 && active == false)
+            if (timer >= chargeTime && active == false)
             {
                 active = true;
                 timer = 0;
+                charging = false;
             }
         }
         if (active == true)
             {
-              beamTime += Time.deltaTime;
-             if (beamTime >= 8)
+            beaming = true; 
+             beamTime += Time.deltaTime;
+             if (beamTimes >= 8)
                 {
                     active = false;
                     beamTime = 0;
+                    beaming = false;
                 }
 
             }
@@ -50,7 +63,7 @@ public class Beam : MonoBehaviour
         if (collision.tag == "Player" && player.GetComponent<PlayerHealth>().invincible == false && active == true)
         {
             player.GetComponent<PlayerHealth>().TakeDamage(1, new Vector2(0, 0));
-
+            active = false;
         }
 
     }
--- Assets/Resources/Scripts/Music.cs
+++ Assets/Resources/Scripts/Music.cs
@@ -9,11 +9,13 @@ public class Music : MonoBehaviour
     public AudioClip mall;
     public AudioClip gwen;
     public AudioClip arcade;
+    public AudioClip miguel;
     public AudioSource audioSource;
     public bool playingHouse = false;
     public bool playingMall = false;
     public bool playingGwen = false;
     public bool playingArcade = false;
+    public bool playingMiguel = false;
     void Start()
     {
 
@@ -23,7 +25,7 @@ public class Music : MonoBehaviour
     void Update()
     {
         DontDestroyOnLoad(this);
-        if((SceneManager.GetActiveScene().name == "House Floor One" || SceneManager.GetActiveScene().name == "House Floor One 1") && !playingHouse)
+        if((SceneManager.GetActiveScene().name == "House Floor One" || SceneManager.GetActiveScene().name == "House Floor One 1" || SceneManager.GetActiveScene().name == "Basement") && !playingHouse)
         {
             audioSource.Stop();
             audioSource.PlayOneShot(house);
@@ -33,8 +35,9 @@ public class Music : MonoBehaviour
             playingMall = false;
             playingGwen = false;
             playingArcade = false;
+            playingMiguel = false;
         }
-        if((SceneManager.GetActiveScene().name == "Mall Level 1") && !playingMall)
+        else if((SceneManager.GetActiveScene().name == "Mall Level 1" || SceneManager.GetActiveScene().name == "Mall Level 2" || SceneManager.GetActiveScene().name == "Mall Level 3" || SceneManager.GetActiveScene().name == "Mall Level 4" || SceneManager.GetActiveScene().name == "Mall Level 6") && !playingMall)
         {
             audioSource.Stop();
             audioSource.PlayOneShot(mall);
@@ -44,8 +47,9 @@ public class Music : MonoBehaviour
             playingHouse = false;
             playingGwen = false;
             playingArcade = false;
+            playingMiguel = false;
         }
-        if(SceneManager.GetActiveScene().name == "Mall Level 5" && !playingGwen)
+        else if(SceneManager.GetActiveScene().name == "Mall Level 5" && !playingGwen)
         {
             audioSource.Stop();
             audioSource.PlayOneShot(gwen);
@@ -55,8 +59,9 @@ public class Music : MonoBehaviour
             playingMall = false;
             playingHouse = false;
             playingArcade = false;
+            playingMiguel = false;
         }
-        if(SceneManager.GetActiveScene().name == "Arcade Level 1" && !playingArcade)
+        else if(SceneManager.GetActiveScene().name == "Arcade Level 1" && !playingArcade || SceneManager.GetActiveScene().name == "Arcade Level 2" || SceneManager.GetActiveScene().name == "Arcade Level 3")
         {
             audioSource.Stop();
             audioSource.PlayOneShot(arcade);
@@ -66,48 +71,21 @@ public class Music : MonoBehaviour
             playingMall = false;
             playingHouse = false;
             playingGwen = false;
+            playingMiguel = false;
         }
-
-
-
-
-
-        if (audioSource.isPlaying == false)
+        else if(SceneManager.GetActiveScene().name == "Arcade Level 4" && !playingMiguel)
         {
-            playingGwen = false;
+            audioSource.Stop();
+            audioSource.PlayOneShot(arcade);
+            audioSource.loop = true;
+            print("played arcade");
+            playingMiguel = true;
             playingMall = false;
             playingHouse = false;
-            if(SceneManager.GetActiveScene().name == "Basement")
-            {
-                audioSource.PlayOneShot(house);
-                audioSource.loop = true;
-                print("played house");
-                playingHouse = true;
-                playingMall = false;
-                playingGwen = false;
-                playingArcade = false;
-            }
-            if(SceneManager.GetActiveScene().name == "Mall Level 2" || SceneManager.GetActiveScene().name == "Mall Level 3" || SceneManager.GetActiveScene().name == "Mall Level 4" || SceneManager.GetActiveScene().name == "Mall Level 6")
-            {
-                audioSource.PlayOneShot(mall);
-                audioSource.loop = true;
-                print("played mall");
-                playingMall = true;
-                playingHouse = false;
-                playingGwen = false;
-                playingArcade = false;
-            }
-            if(SceneManager.GetActiveScene().name == "Arcade Level 2" || SceneManager.GetActiveScene().name == "Arcade Level 3")
-            {
-                audioSource.PlayOneShot(arcade);
-                audioSource.loop = true;
-                print("played arcade");
-                playingArcade = true;
-                playingMall = false;
-                playingHouse = false;
-                playingGwen = false;
-            }
+            playingGwen = false;
+            playingArcade = false;
         }
+
 
     }
 }
