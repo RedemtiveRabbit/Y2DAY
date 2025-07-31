fe0d2c5977ce479ddf191bb1897a6d729de30ac5
EzraF8 <ezrafindlay@gmail.com>
2025-07-31 19:47:40 -0400

    63

--- Assets/Resources/Scripts/SaveData.cs
+++ Assets/Resources/Scripts/SaveData.cs
@@ -7,6 +7,8 @@ public class SaveData
     public int hasCouch = -1;
     public int hasArcadeMachine = -1;
     public int hasShelf = -1;
+    public int hasPlant = -1;
+    public int hasTable = -1;
     public int levelsCompleted = 0;
     public static SaveData current = new();
 
