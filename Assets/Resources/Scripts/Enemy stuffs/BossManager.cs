b51307e2cae5196cd290c860055cd98547a2413e
Jonathan Saunders <jonathanedsaunders@gmail.com>
2025-08-01 15:01:59 -0400

    jkjkj

--- Assets/Resources/Scripts/Enemy stuffs/BossManager.cs
+++ Assets/Resources/Scripts/Enemy stuffs/BossManager.cs
@@ -6,11 +6,15 @@ public class BossManager : MonoBehaviour
 {
     public int phase;
     public bool stun;
+    public bool walking;
+    public bool attacking;
     public GameObject player;
     public GameObject bullet;
     public Transform bulletPos;
     public Health health;
     public Animator animator;
+    public AI_Chase aiChase;
+    public ShootTurret shootTurret;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -20,34 +24,48 @@ public class BossManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector2(player.transform.position.x, 4);
         float distance = Vector2.Distance(transform.position, player.transform.position);
         Vector3 direction = (player.transform.position - transform.position).normalized;
         Quaternion targetRotation = Quaternion.LookRotation(Vector3.forward, direction);
 
-        if (health.HP < 6 && health.HP > 3)
+        if (health.HP <= 11 && health.HP > 6)
         {
             phase = 2;
         }
-        if (health.HP < 3)
+        else if (health.HP <= 6 && health.HP > 1)
         {
             phase = 3;
         }
-        if (phase == 1)
+        else if(health.HP == 1)
         {
-
-        }
-        else if (phase == 2)
-        {
-
-        }
-        else if (phase == 3)
-        {
-
+            phase = 4;
         }
         
-         animator.SetFloat("DirectionX", direction.x);
-         animator.SetFloat("DirectionY", direction.y);
+        if((aiChase.body.linearVelocityX != 0 || aiChase.body.linearVelocityY != 0) && !attacking)
+        {
+            walking = true;
+        }
+        else
+        {
+            walking = false;
+        }
+
+        if(!shootTurret.coolingDown)
+        {
+            attacking = true;
+        }
+        else
+        {
+            attacking = false;
+        }
+
+
+
+        animator.SetFloat("DirectionX", direction.x);
+        animator.SetFloat("DirectionY", direction.y);
+        animator.SetInteger("Stage", phase);
+        animator.SetBool("Walking", walking);
+        animator.SetBool("Attacking", attacking);
 
          
 
